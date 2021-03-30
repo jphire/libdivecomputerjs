6c844bf893263ecab9356f9210c0095d086389b1
Vincent Hagen <vinnie@script4web.nl>
2021-03-30 20:51:24 +0200

    Made cmake work on windows

--- cmake/libdivecomputer.cmake
+++ cmake/libdivecomputer.cmake
@@ -3,17 +3,21 @@ IF(NOT WIN32)
     include(ExternalProject)  
     ExternalProject_Add(libdivecomputer
         URL https://github.com/libdivecomputer/libdivecomputer/archive/refs/heads/master.zip
-        CONFIGURE_COMMAND autoreconf --install && ./configure --prefix=${CMAKE_CURRENT_SOURCE_DIR}/deps/libdivecomputer
+        CONFIGURE_COMMAND autoreconf --install && ./configure --prefix=${CMAKE_CURRENT_SOURCE_DIR}/deps/
         BUILD_IN_SOURCE TRUE
     )
 ENDIF()
 
 IF(WIN32)
-    file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/dev/headers DESTINATION ${CMAKE_CURRENT_SOURCE_DIR}/deps/include)
+    file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/dev/libdivecomputer DESTINATION ${CMAKE_CURRENT_SOURCE_DIR}/deps/include)
     add_custom_target(libdivecomputer
         COMMAND node ${CMAKE_CURRENT_SOURCE_DIR}/dev/download-for-win32.js ${CMAKE_CURRENT_SOURCE_DIR}/deps
     )
+    add_library(divecomputer SHARED IMPORTED)
+    set_property(TARGET divecomputer PROPERTY IMPORTED_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/deps/lib/libdivecomputer-0.dll")
+    set_property(TARGET divecomputer PROPERTY IMPORTED_IMPLIB "${CMAKE_CURRENT_SOURCE_DIR}/deps/lib/libdivecomputer-0.lib")
+    set_target_properties(divecomputer PROPERTIES LINKER_LANGUAGE C)
 ENDIF()
 
-include_directories(${CMAKE_SOURCE_DIR}/deps/libdivecomputer/include)
-link_directories(${CMAKE_SOURCE_DIR}/deps/libdivecomputer/lib)
+include_directories(${CMAKE_SOURCE_DIR}/deps/include)
+link_directories(${CMAKE_SOURCE_DIR}/deps/lib)
